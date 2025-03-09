3bbe79a22f7ff98f51341235356b4aaa79a4f657
Mashhood <mashhoodakomr@gmail.com>
2025-03-09 15:20:01 +0530

    git

---
+++ src/GIT/git.js
@@ -0,0 +1,4 @@
+// rename branch :git branch -m new name (type in exist branhc)
+// git add -p :for review and add to stage
+// git commit --amend :for edit commit message
+// git commit --amend --no-edit :for edit commit message
